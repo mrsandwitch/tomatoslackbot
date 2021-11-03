ce5a7f8fde0dbc9a999fce5c80045d9a71ecc0ba
bushyang <bushyang@aetherai.com>
2021-11-03 17:29:49 +0800

    Add cors header and test serving vue compiled result

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/cors v1.2.0 // indirect
 	github.com/jinzhu/gorm v1.9.16
 	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 )
 
 var ip = flag.String("ip", "0.0.0.0", "server ip")
@@ -20,10 +21,15 @@ var port = flag.Int("port", 8000, "server port")
 
 //go:embed web_app/templates/*
 var templates embed.FS
+
 //go:embed web_app/assets/*
 var assets embed.FS
+
 //go:embed web_app/src/*
 var webSrc embed.FS
+
+//go:embed dist/*
+var webDist embed.FS
 
 func main() {
 	flag.Parse()
@@ -43,11 +49,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	templateRootFs, err := fs.Sub(templates, "web_app")
+	templateRootFs, err := fs.Sub(templates, "web_app/templates")
 	if err != nil {
 		log.Fatal(err)
 	}
 	webSrcRootFs, err := fs.Sub(webSrc, "web_app")
+	if err != nil {
+		log.Fatal(err)
+	}
+	webDistRootFs, err := fs.Sub(webDist, "dist")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,10 +67,16 @@ func main() {
 	clockService := service.InitClockService(senderService, dbService, confService)
 	webService := service.InitWebviewService(senderService, dbService, templateRootFs)
 
-	//-- Test function
-	//senderService.SendMsg("hello2")
-	//clockService.Start()
+	corsHandler := cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
 
+	r.Use(corsHandler)
 	r.Group(func(r chi.Router) {
 		{
 			r.Post("/tomato", clockService.TomatoClockStart)
@@ -68,9 +84,12 @@ func main() {
 			r.Post("/setting", confService.Setting)
 			//r.Get("/record", webService.RecordPage)
 			//r.Get("/clock", webService.ClockPage)
-			r.Get("/test", webService.TestPage)
+			//r.Get("/test", webService.TestPage)
 			r.Handle("/assets/*", http.FileServer(http.FS(assetRootFs)))
 			r.Handle("/src/*", http.FileServer(http.FS(webSrcRootFs)))
+			//r.Handle("/", http.FileServer(http.FS(templateRootFs)))
+			r.Handle("/*", http.FileServer(http.FS(webDistRootFs)))
+			r.Get("/api/records", webService.RecordGet)
 		}
 	})
 
