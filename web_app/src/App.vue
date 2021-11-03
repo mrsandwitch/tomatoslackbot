bc58883f63dcf2dd5f5c233ed8fe4959cc2380c5
bushyang <bushyang@aetherai.com>
2021-11-03 17:49:20 +0800

    Use vue framework for web app

---
+++ web_app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Clock/>
+</template>
+
+<script>
+import Clock from './components/Clock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Clock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ web_app/src/components/Clock.vue
@@ -0,0 +1,163 @@
+<template>
+  <iframe name="dummyframe" id="dummyframe" style="display: none"></iframe>
+
+  <div class="clock-container">
+    <form action="/tomato" method="post" target="dummyframe">
+      <div style="padding: 10px">
+        <button class="uk-button uk-button-default" name="ctlStr" value="w 10m">
+          10 (work)
+        </button>
+        <button
+          class="uk-button uk-button-primary"
+          name="ctlStr"
+          value="s 10m"
+          style="margin-left: 10px"
+        >
+          10 (spare)
+        </button>
+      </div>
+      <div style="padding: 10px">
+        <button class="uk-button uk-button-default" name="ctlStr" value="w 25m">
+          25 (work)
+        </button>
+        <button
+          class="uk-button uk-button-primary"
+          name="ctlStr"
+          value="s 25m"
+          style="margin-left: 10px"
+        >
+          25 (spare)
+        </button>
+      </div>
+    </form>
+  </div>
+
+  <hr class="uk-divider-icon" />
+
+  <div class="ongoing-clock-container">
+    <div class="ongoing-clock-row">
+      <div uk-countdown="date: 2021-10-10T12:27:56+00:00">
+        <span class="uk-countdown-number uk-countdown-minutes"></span>
+        :
+        <span class="uk-countdown-number uk-countdown-seconds"></span>
+      </div>
+      <button class="uk-button uk-button-primary" name="stop" value="stop">
+        stop
+      </button>
+    </div>
+    <div class="ongoing-clock-row">
+      <div uk-countdown="date: 2021-10-10T12:27:56+00:00">
+        <span class="uk-countdown-number uk-countdown-minutes"></span>
+        :
+        <span class="uk-countdown-number uk-countdown-seconds"></span>
+      </div>
+      <button class="uk-button uk-button-primary" name="stop" value="stop">
+        stop
+      </button>
+    </div>
+  </div>
+
+  <div class="uk-child-width-1-3@m uk-child-width-1-2@s records-container">
+    <div v-for="item in items" v-bind:key="item" class="record-card-container">
+      <div class="uk-card uk-card-default uk-card-body">
+        <h3 class="uk-card-title">
+          <div class="uk-card-badge uk-label">{{ item.Count }}</div>
+          {{ item.Title }}
+        </h3>
+        <table class="uk-table uk-table-small uk-text-nowrap">
+          <tbody>
+            <tr v-for="readable in item.Readables" v-bind:key="readable">
+              <td class="uk-width-1-4">{{ readable.Start }}</td>
+              <td class="uk-width-1-4">{{ readable.Duration }}</td>
+              <td v-bind:class="'uk-text-' + readable.Label">
+                {{ readable.Tag }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+export default {
+  //   name: "Clock",
+  //   props: {
+  //     msg: String,
+  //   },
+  //data() {
+  //  //return {
+  //  //  items: null,
+  //  //};
+  //},
+  setup() {
+    const items = ref([]);
+
+    const getRecords = async () => {
+      try {
+        items.value = await axios
+          .get(`http://localhost:8000/api/records`)
+          .then((resp) => resp.data.Items);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    onMounted(() => {
+      getRecords();
+    });
+
+    return {
+      items,
+    };
+  },
+  methods: {
+    test2() {
+      axios
+        .get(`http://localhost:8000/api/records`)
+        .then((resp) => (this.items = resp.data.items));
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.clock-container {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.ongoing-clock-container {
+  padding: 0px 25px 25px;
+}
+
+.uk-countdown-number {
+  font-size: 1rem;
+}
+
+.ongoing-clock-row {
+  font-size: 1rem;
+  display: flex;
+  padding: 10px;
+  gap: 25px;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.record-card-container {
+  padding: 25px;
+}
+
+.records-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
